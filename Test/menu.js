149a2424ac49ef8d4a59519cae083f9ca4c646bd
seannc <seanncunningham@users.noreply.github.com>
2018-02-20 22:42:21 -0800

    Test

---
+++ Test/menu.js
@@ -0,0 +1,7 @@
+document.getElementById("open").addEventListener("click",function(){
+    document.getElementById("menu").style.left = "0px";
+});
+
+document.getElementById("close").addEventListener("click",function(){
+    document.getElementById("menu").style.left = "-110px";
+});
