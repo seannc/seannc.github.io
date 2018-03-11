bded8de4f90f1403764736dc064f6d4c29ab54cc
seannc <seanncunningham@users.noreply.github.com>
2018-03-11 16:54:23 -0700

    Smash

---
+++ Infinite/index.js
@@ -0,0 +1,9 @@
+/* document.getElementById("profile").addEventListener("mouseover", function(){ 
+   //document.getElementById("profile").style.transform = "scale(1.1,1.1)";
+   document.getElementById("title").style.opacity = "1";
+});
+
+document.getElementById("profile").addEventListener("mouseout", function(){ 
+   //document.getElementById("profile").style.transform = "scale(1.1,1.1)";
+   document.getElementById("title").style.opacity = "0";
+});
